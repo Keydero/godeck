b35fd682f3466eb31dfebe0f0aa38dd221c29fb8
keyproco <oussama.keddar@epitech.eu>
2023-06-19 23:50:05 +0200

    wow

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 
 	cards = append(cards, "Six Of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
