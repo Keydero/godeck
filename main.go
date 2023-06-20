9cfd748ebf5799cca5937e648ccbf968d835e0a8
keyproco <oussama.keddar@epitech.eu>
2023-06-20 20:55:30 +0200

    Returning multiple values

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	//cards.print()
+
+	//fmt.Println(cards[:3])
+
+	hand, remainingCards := deal(cards, 5)
+	remainingCards.print()
+	hand.print()
+
 }
