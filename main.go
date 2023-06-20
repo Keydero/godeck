4c7ed9675e4f955e53dd90f88f872ff27ce3cb05
keyproco <oussama.keddar@epitech.eu>
2023-06-20 11:39:54 +0200

    newdeck function
    it loops over the cards suits and assign a value for each from cardValues list

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-
-	cards = append(cards, "Six Of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five Of Diamonds"
 }
